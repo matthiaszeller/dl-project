8834a403391a686b0305ce346c9b136cd42e26dd
Matthias Zeller <m.zeller1901@gmail.com>
2021-03-15 09:11:32 +0100

    MWE: auxiliary loss & plot stats for multiple runs

--- Proj1/stats.py
+++ Proj1/stats.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 import torch.nn.functional as F
 from torch import optim
 
 from training import train
 
 
-def train_multiple_runs(network_class, runs=10, epoch=30):
+def train_multiple_runs(network_class, criterion=F.binary_cross_entropy, runs=10, epoch=30):
     all_train_loss, all_train_acc, all_test_loss, all_test_acc = [], [], [], []
 
     for i in range(runs):
         n = network_class()
         optimizer = optim.SGD(n.parameters(), lr=0.01, momentum=0.5)
-        criterion = F.binary_cross_entropy
 
         tot_train_loss, tot_train_acc, tot_test_loss, tot_test_acc = train(n, optimizer, criterion, epoch, debug_=False)
         all_train_loss.append(tot_train_loss)
         all_train_acc.append(tot_train_acc)
         all_test_loss.append(tot_test_loss)
         all_test_acc.append(tot_test_acc)
+
+        if i % 2 == 0:
+            print(i, end=' ')
 
     return all_train_loss, all_train_acc, all_test_loss, all_test_acc
 
@@ -38,23 +41,37 @@ def plot_loss_acc(tot_train_loss, tot_train_acc, tot_test_loss, tot_test_acc):
 
 
 def plot_std_loss_acc(all_train_loss, all_train_acc, all_test_loss, all_test_acc):
-    trl_mean = np.array(all_train_loss).mean(axis=0)
-    tel_mean = np.array(all_train_acc).mean(axis=0)
-    tra_mean = np.array(all_test_loss).mean(axis=0)
-    tea_mean = np.array(all_test_acc).mean(axis=0)
+    def subplot(ax, mean, std, label=None):
+        x = range(1, len(mean) + 1)
+        ax.plot(x, mean, label=label)
+        ax.fill_between(x, mean - std, mean + std, alpha=0.3)
 
-    trl_std = np.array(all_train_loss).std(axis=0)
-    tel_std = np.array(all_train_acc).std(axis=0)
-    tra_std = np.array(all_test_loss).std(axis=0)
-    tea_std = np.array(all_test_acc).std(axis=0)
+    # Convert lists of lists to tensors
+    data = {
+        'loss_train': all_train_loss,
+        'loss_test': all_test_loss,
+        'acc_train': all_train_acc,
+        'acc_test': all_test_acc
+    }
 
-    epochs = range(1, len(tea_std) + 1)
+    data = {k: torch.tensor(v) for k, v in data.items()}
 
-    temp = [[trl_mean, trl_std], [tel_mean, tel_std], [tra_mean, tra_std], [tea_mean, tea_std]]
+    # Compute mean and std. The multiple runs are in axis 0, epochs in axis 1
+    plot_data = {}
+    for label, tensor in data.items():
+        plot_data[f'{label}_mean'] = tensor.mean(dim=0)
+        plot_data[f'{label}_std'] = tensor.std(dim=0)
 
-    for g in temp:
-        plt.plot(epochs, g[0])
-        plt.fill_between(epochs, g[0] - g[1], g[0] + g[1], alpha=0.3)
+    # Plot!
+    fig, ax = plt.subplots(2, 1, sharex=True, figsize=(6, 5))
+    subplot(ax[0], plot_data['loss_train_mean'], plot_data['loss_train_std'], label='train')
+    subplot(ax[0], plot_data['loss_test_mean'], plot_data['loss_test_std'], label='test')
+    ax[0].set_ylabel('Loss')
+    subplot(ax[1], plot_data['acc_train_mean'], plot_data['acc_train_std'])
+    subplot(ax[1], plot_data['acc_test_mean'], plot_data['acc_test_std'])
+    ax[1].set_ylabel('Accuracy')
+    ax[1].set_xlabel('Epoch')
 
-    plt.ylim((-0.1, 1.1))
+    ax[0].legend()
+    plt.show()
 
--- Proj1/test.py
+++ Proj1/test.py
@@ -3,14 +3,28 @@
 import torch.nn.functional as F
 from torch import optim
 
-from nets import FullyDenseNet
+from loss import custom_loss
+from nets import FullyDenseNet, FullyDenseNetAux
+from stats import train_multiple_runs, plot_std_loss_acc
 from training import train
+
+import matplotlib.pyplot as plt
 
 
 def main():
     net = FullyDenseNet()
     optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.5)
     tot_train_loss, tot_train_acc, tot_test_loss, tot_test_acc = train(net, optimizer, F.binary_cross_entropy)
+
+    net = FullyDenseNetAux()
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.5)
+    tot_train_loss, tot_train_acc, tot_test_loss, tot_test_acc = train(net, optimizer, criterion_=custom_loss)
+
+    all_train_loss, all_train_acc, all_test_loss, all_test_acc = train_multiple_runs(FullyDenseNetAux,
+                                                                                     criterion=custom_loss,
+                                                                                     epoch=25,
+                                                                                     runs=15)
+    plot_std_loss_acc(all_train_loss, all_train_acc, all_test_loss, all_test_acc)
 
 
 if __name__ == '__main__':
