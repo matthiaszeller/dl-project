587d776d9a3b255ab0705855444e89f8ed372554
Fatih <fatih.mutlu@epfl.ch>
2021-05-01 11:38:02 +0200

    MSE Implemented

--- Proj2/function.py
+++ Proj2/function.py
@@ -47,4 +47,29 @@ class tanh(ActivationFunction):
    def __init__(self):
         super().__init__()
         self.f =  lambda *x: tuple(map(lambda y: np.tanh(y), x))
-        self.df = lambda *x: tuple(map(lambda y: 1-np.power(np.tanh(y),2), x)
+        self.df = lambda *x: tuple(map(lambda y: 1-np.power(np.tanh(y),2), x))
+
+class Loss(Module):
+    #reduction: str
+    
+    def __init__(self, reduction:str ='mean'):
+        super(Module, self).__init__()
+        self.reduction = reduction
+        print(self.reduction)
+
+class LossMSE(Loss):
+    def __init__(self, reduction = 'mean'):
+        super(LossMSE, self).__init__(reduction)
+
+    def forward(self, input, target) :
+        super(LossMSE, self).forward()
+            
+        e = (input - target)**2
+        if self.reduction == 'none' :
+            return e
+        elif self.reduction == 'mean':
+            return torch.mean(e)
+        elif self.reduction == 'sum':
+            return torch.sum(e)
+        else:
+            return e
