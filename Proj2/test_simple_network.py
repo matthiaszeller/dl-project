262291e14d9cf7834fee158d477af0c5a0e005f6
Matthias Zeller <m.zeller1901@gmail.com>
2021-05-10 21:29:58 +0200

    simple test of framework

--- Proj2/test_simple_network.py
+++ Proj2/test_simple_network.py
@@ -1,4 +1,4 @@
-
+from time import time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -62,16 +62,18 @@ y = Xtest @ wstar
 plt.plot(hgrid, y, label='regline gender=0')
 
 
-
 class Dataset:
 
-    def __init__(self, data, target):
+    def __init__(self, data, target, shuffle=False):
         """data must be an n times d matrix, n = number of samples, d = number of features."""
         self.data = data
         self.target = target
+        self.shuffle = shuffle
 
     def __iter__(self):
-        for i in range(self.data.shape[0]):
+        n = self.data.shape[0]
+        iterator = np.random.permutation(n) if self.shuffle else range(n)
+        for i in iterator:
             # Take i-th row and instantiate Tensor
             x = Tensor(self.data[i])
             y = Tensor(self.target[i])
@@ -79,54 +81,108 @@ class Dataset:
             yield x, y
 
 
-def train_epoch_sgd(dataset: Dataset, model: Module, loss_fun: Function, lr: float):
+def train_epoch_sgd(dataset: Dataset, model: Module, loss_fun: Function, lr: float, lambda_: float):
     losses = []
+    t = time()
+
     for i, (x, y) in enumerate(dataset):
-        model.zero_grad()
         # Forward pass
+        model.zero_grad()
         output = model(x)
         loss = loss_fun(output, y)
         # Backward pass
         loss.backward()
-        # Gradient step
-        model.step(lr)
-
         losses.append(loss.item())
+        model.step(lr)
+    a = 0
 
-    return losses
+    # Regularization
+    #model.zero_grad()
+    # loss = Tensor(0.0)
+    # for p in model._params():
+    #     loss += (p * p).sum()
+    # loss *= Tensor(lambda_)
+    # loss.backward()
+    # model.step(lr)
+    weights_norm = {
+        p._name: p.data.pow(2).sum() for p in model._params()
+    }
+
+    t = time() - t
+    data = {
+        'loss': losses,
+        'time': t,
+        'weight': weights_norm
+    }
+    return data
 
 
-def train_sgd(dataset: Dataset, model: Module, loss_fun: Function, lr: float, epochs: int):
+def train_sgd(dataset: Dataset, model: Module, loss_fun: Function, lr: float, epochs: int, lambda_: float = 0):
     losses = []
+    times = []
+    weights = []
     for epoch in range(1, epochs+1):
-        loss_epoch = train_epoch_sgd(dataset, model, loss_fun, lr)
-        losses.append(torch.tensor(loss_epoch).mean().item())
+        data = train_epoch_sgd(dataset, model, loss_fun, lr, lambda_)
+        losses.append(torch.tensor(data['loss']).mean().item())
+        times.append(data['time'])
+        weights.append(data['weight'])
 
-    return losses
+    data = {
+        'loss': losses,
+        'time': times,
+        'weight': weights
+    }
+    return data
 
 
 X = np.vstack((h, g)).T
+
+# Data normalization
+mu, std = X.mean(0), X.std(0)
+X = (X - mu) / std
+
 dataset = Dataset(X, w)
 
 s = Sequential(
-    LinearLayer(2, 2),
-    #ReLU(),
     LinearLayer(2, 1)
 )
 
-# Smoothness constant
-L = np.linalg.eigvals(2 * X.T @ X).max()
-losses = train_sgd(dataset, s, MSELoss(), lr=0.01, epochs=10)
+# Smoothness constant and strongly-convex constant
+eigs = np.linalg.eigvals(2 * X.T @ X)
+L = max(eigs)
+mu_sc = min(eigs)
+
+data = train_sgd(dataset, s, MSELoss(), lr=1/L, epochs=10)
+losses = data['loss']
 
 # Manual sanity check
-Xtest = Xtest[:, 1:]
+# For gender = 1
+Xtest = np.ones((100, 2))
+Xtest[:, 0] = hgrid
+Xtest = (Xtest - mu) / std
 testset = Dataset(Xtest, w)
 
 y = np.array([
     s(x).item() for x, _ in testset
 ])
 
-plt.plot(hgrid, y, label='network, gender=0')
+plt.plot(hgrid, y, label='1 layer, gender=1')
+
+
+# --- More complicated network
+s = Sequential(
+    LinearLayer(2, 2),
+    LinearLayer(2, 1)
+)
+
+dataset = Dataset(X, w)
+data = train_sgd(dataset, s, MSELoss(), lr=1/L, epochs=10)
+y = np.array([
+    s(x).item() for x, _ in testset
+])
+
+plt.plot(hgrid, y, label='2 linear layers, no non-linearity')
+
 plt.legend()
 plt.show()
 
